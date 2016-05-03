217257781b3689a815744193ab4e1867cf9b51cb
Lazarescu Andrei <andrei_spion@yahoo.com>
2016-05-03 17:57:21 +0300

    Cleaned and optimized JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,70 +4,18 @@
 //= require turbolinks
 //= require cocoon
 /*global $*/
-//= require_tree .
 $(() => {
-$(".container > .project .feedbacks .reply").click(function(e) {
-  
-  $(e.target).parents(".feedback-content").next().children(".reply-form").toggle();
-});
-$(".container > .project .description .more").click(function() {
-  $(".container > .project .description .more").hide();
-  if($(".container > .project .description .complete").length > 0) {
-    $(".container > .project .description .first140").hide();
-    $(".container > .project .description .complete").show();
-    if ($(".container > .project .description .carousel").length > 0) {
-      $(".container > .project .description .carousel").show();
-    }
-  } else if ($(".container > .project .description .carousel").length > 0) {
-    $(".container > .project .description .carousel").show();
-  }
-});
-$(".container > .project .members .more").click(function() {
-  $(".container > .project .members .more").hide();
-  $(".container > .project .members .hidden").show();
-});
-$("header .person .settings").click(function () {
-  $("header .person .settings-dropdown").toggle();
-});
-$("header .person .notification-icon").click(function () {
-  $("header .person svg.notification-icon").children('path#bg').css("fill", "#efefef");
-  $("header .person .notification-dropdown").toggle();
-});
-var $carousel = $(".image-carousel");
-$carousel.find(".carousel-image-container").eq(0).addClass("active");
-$carousel.children(".arrow-right").click(function() {
-  var $active = $carousel.find(".active");
-  if($active.index() + 1 < $carousel.children(".carousel-images").children().length) {
-    $active.next().addClass("active");
-    $active.removeClass("active");
-  }
-});
-$carousel.children(".arrow-left").click(function() {
-  var $active = $carousel.find(".active");
-  if($active.index() > 0) {
-    $active.prev().addClass("active");
-    $active.removeClass("active");
-  }
-});
-$carousel.find(".carousel-image-container").click(function(ev) {
-  var src = $(ev.target).attr("src");
-  openLightbox(src);
-});
-var $carouselControl = $(".carousel-control");
-$carouselControl.children(".carousel-control-image-container").click(function(ev) {
-  var $active = $carousel.find(".active");
-  $active.removeClass("active");
-  $carousel.find(".carousel-image-container").eq($(ev.target).parent().index()).addClass("active");
-});
-});
+  var $person = $("header .person")
+  var $settingsIcon = $person.find(".settings");
+  var $settingsDropdown = $person.find(".settings-dropdown");
+  var $notificationIcon = $person.find(".notification-icon");
+  var $notificationDropdown = $person.find(".notification-dropdown");
 
-function openLightbox (img_src) {
-  var $lightbox = $(".lightbox");
-  console.log($lightbox.find(".lightbox-image"));
-  $lightbox.find(".lightbox-image").attr("src", img_src);
-  $lightbox.show();
-  $(window).keydown(function(e) {
-    if(e.keyCode === 27)
-      $lightbox.hide();
+  $settingsIcon.click(function () {
+    $settingsDropdown.toggle();
   });
-}
+  $notificationIcon.click(function () {
+    $notificationIcon.children('path#bg').css("fill", "#efefef");
+    $notificationDropdown.toggle();
+  });
+});
---
+++ app/assets/javascripts/projects.js
@@ -0,0 +1,79 @@
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require turbolinks
+//= require cocoon
+/*global $*/
+$(() => {
+  var $project = $(".container > .project");
+  var $lightbox = $project.siblings(".lightbox");
+  var $lightboxImage = $lightbox.find(".lightbox-image");
+  var $replyButtons = $project.find(".feedbacks .reply");
+  var $description = $project.find(".description-content");
+  var $moreDesc = $description.children(".more");
+  var $complete = $description.children(".complete");
+  var $first140 = $description.children(".first140");
+  var $carousel = $description.children(".carousel");
+  var $carouselImages = $carousel.find("carousel-image-container");
+  var $carouselControl = $carousel.children(".carousel-control");
+  var $carouselControlImages = $carouselControl.children(".carousel-control-image-container");
+  var $members = $project.children("members");
+  var $moreMembers = $members.find(".more");
+  var $hiddenMembers = $members.find(".hidden");
+
+  $replyButtons.click(openReplyBox);
+  $moreDesc.click(function() {
+    $moreDesc.hide();
+    if($complete.length > 0) {
+      $first140.hide();
+      $complete.show();
+      if ($carousel.length > 0) {
+        $carousel.show();
+      }
+    } else if ($carousel.length > 0) {
+      $carousel.show();
+    }
+  });
+  $moreMembers.click(function() {
+    $moreMembers.hide();
+    $hiddenMembers.show();
+  });
+  $carouselImages.eq(0).addClass("active");
+  $carousel.children(".arrow-right").click(function() {
+    var $active = $carouselImages.siblings().find(".active");
+    if($active.index() + 1 < $carouselImages.length) {
+      $active.next().addClass("active");
+      $active.removeClass("active");
+    }
+  });
+  $carousel.children(".arrow-left").click(function() {
+    var $active = $carouselImages.siblings().find(".active");
+    if($active.index() > 0) {
+      $active.prev().addClass("active");
+      $active.removeClass("active");
+    }
+  });
+  $carouselImages.click(function(ev) {
+    var src = $(ev.target).attr("src");
+    openLightbox(src);
+  });
+  $carouselControlImages.click(function(ev) {
+    var $active = $carouselImages.siblings().find(".active");
+    $active.removeClass("active");
+    $carouselImages.eq($(ev.target).parent().index()).addClass("active");
+  });
+
+  function openLightbox (img_src) {
+    $lightboxImage.attr("src", img_src);
+    $lightbox.show();
+    $(window).keydown(function(e) {
+      if(e.keyCode === 27)
+        $lightbox.hide();
+    });
+  }
+
+  function openReplyBox(e) {
+    $(e.target).parents(".feedback-content").next().children(".reply-form").toggle();
+  }
+
+});
